7e607bdd6cf3bb3ee93ecfd656b855c76b329d10
Samson Hill <samsonhill@Samsons-MacBook-Air.local>
2024-11-20 17:13:08 -0800

    pet-salon

--- scripts/register.js
+++ scripts/register.js
@@ -8,12 +8,32 @@ let petSalon = {
     }
 }
 //object constructor
-function Pet(name,age,gender,breed,service){
+function Pet(name,age,gender,breed,service,petType){
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
     this.servive=service;
+    this.petType=petType;
+}
+
+//validation
+function isValid(pet){
+    let validation =true;
+    let inputName = document.getElementById("txtName");
+    let inputAge = document.getElementById("txtAge");
+
+    if(pet.name==""){
+        validation=false;
+        inputName.classList.add("error");
+    }
+
+    if(pet.age==""){
+        validation=false;
+        inputAge.classList.add("error");
+    }
+
+    return validation
 }
 
 //add breed and service
@@ -29,11 +49,36 @@ function register(){
     let inputPetType=document.getElementById("txtPetType").value;
 
     console.log(inputName,inputAge,inputGender,inputBreed,inputService,inputPetType);
+
     //create the object
     let newPet = new Pet(inputName,inputAge,inputGender,inputBreed,inputService,inputPetType);
-    //push the obj to the array
+
+    //check validations
+    if(isValid(newPet) == true) {
+         //push the obj to the array
     pets.push(newPet);
+    //clear an input: document.getElementById("txtName").value="";
+    //display the obj on the console
+    console.log(pets);
+    displayPet();
+
+    clearForm();
     //clear an input document.getElementById("txtName").value="";
+    
+    }
+
+
+
+    function clearForm(){
+        document.getElementById("txtName").value="";
+        document.getElementById("txtAge").value="";
+        document.getElementById("txtGender").value="";
+        document.getElementById("txtBreed").value="";
+        document.getElementById("txtService").value="";
+        document.getElementById("txtPetType").value="";
+
+    }
+    
     //display the obj on the console
     console.log(pets);
 }
@@ -45,8 +90,15 @@ function init(){
     //create two more pets
     let pet2 = new Pet("Scrappy",5,"Male","German Shepard","dog spa");
     let pet3 = new Pet("Midnight",8,"Female","Pitbull","Bath");
+    pets.push(pet1,pet2,pet3);
+
     console.log(pet1,pet2,pet3);
+
+    displayPet();
 }
+
+
+
 
 window.onload=init;//wait to render the HTML
 
